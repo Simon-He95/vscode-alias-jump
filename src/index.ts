9ffc32c6cfbd2eaf8e40ae7ba47a9ace3d410f1c
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-08-29 10:18:45 +0800

    chore: add useJSONParse 修复可能不规则的JSON

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import path from 'node:path'
 import * as vscode from 'vscode'
 import { findAliasExtensions, getAlias, getRootUrl, getUrl } from './utils'
-import a from './a'
 
 // todo: 支持monorepo
 export function activate(context: vscode.ExtensionContext) {
--- src/utils.ts
+++ src/utils.ts
@@ -22,9 +22,9 @@ export function getAlias() {
     const tsConfig = `${rootUrl}/tsconfig.json`
     const jsConfig = `${rootUrl}/jsconfig.json`
     const json = fs.existsSync(tsConfig)
-      ? JSON.parse(fs.readFileSync(tsConfig, 'utf-8'))
+      ? useJSONParse(fs.readFileSync(tsConfig, 'utf-8'))
       : fs.existsSync(jsConfig)
-        ? JSON.parse(fs.readFileSync(jsConfig, 'utf-8'))
+        ? useJSONParse(fs.readFileSync(jsConfig, 'utf-8'))
         : undefined
     if (!json)
       return
@@ -68,4 +68,27 @@ function isFile(url: string) {
   catch (error) {
     return false
   }
+}
+
+const keyReg = /\s+(?!['"])([\w_\-\$\.]+):/gm
+const valueReg = /:\s*(?!['"])([\w_\-\$\.]+)/gm
+const commaLackReg = /:\s*("[\w_\-\$\.]+")\s*(?!,)"/gm
+const commaMoreReg = /:\s*("[\w_\-\$\.]+"\s*,)\s*}/gm
+const moreCommaReg = /,(\s*})/gm
+/**
+ * 将字符串转为JSON.stringify的格式并parse出结果
+ * @param { string } str 字符串
+ * @returns
+ */
+export function useJSONParse(str: string) {
+  return JSON.parse(
+    str
+      .replace(keyReg, (match, key) => match.replace(key, `"${key}"`))
+      .replace(valueReg, (match, value) => match.replace(value, `"${value}"`))
+      .replace(commaLackReg, (match, value) =>
+        match.replace(value, `${value},`),
+      )
+      .replace(commaMoreReg, match => match.replace(',', ''))
+      .replace(moreCommaReg, (_, v) => v),
+  )
 }
